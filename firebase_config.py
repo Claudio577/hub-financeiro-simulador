2622d63db35461389e1aba57e7e04d744f03a6fe
Claudio577 <chideki.yoshida@gmail.com>
2025-11-27 06:48:36 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-# Carrega o arquivo da chave privada
-cred = credentials.Certificate("firebase-key.json")
+# Carrega a chave que está nos Secrets do Streamlit
+firebase_key_json = os.environ.get("FIREBASE_KEY_JSON")
 
-# Inicializa o Firebase Admin SDK
-firebase_admin.initialize_app(cred)
+if not firebase_key_json:
+    raise ValueError("Chave FIREBASE_KEY_JSON não encontrada nos secrets do Streamlit!")
 
-# Cria o cliente Firestore
+# Converte o texto JSON dos secrets para um dicionário Python
+firebase_key_dict = json.loads(firebase_key_json)
+
+# Cria a credencial Firebase a partir do dicionário
+cred = credentials.Certificate(firebase_key_dict)
+
+# Inicializa o Firebase se ainda não tiver sido inicializado
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+# Conexão com o Firestore
 db = firestore.client()
