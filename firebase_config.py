340b55b609db1b46e66d34e15d6d8d4c833c37df
Claudio577 <chideki.yoshida@gmail.com>
2025-11-27 06:27:53 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,1 +1,11 @@
-# Configuração Firebase (será preenchido depois)
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Carrega o arquivo da chave privada
+cred = credentials.Certificate("firebase-key.json")
+
+# Inicializa o Firebase Admin SDK
+firebase_admin.initialize_app(cred)
+
+# Cria o cliente Firestore
+db = firestore.client()
